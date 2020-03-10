60b328de4110a634ede54fdfe97d31022ddf1bd3
snerks <martin.github@outlook.com>
2020-03-10 20:24:27 +0000

    Updates

---
+++ precache-manifest.43bdb4f5c61e0e683fde8a1635b242a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb788f88b1f1b7bd1ea489c6285b1e2a",
+    "url": "/react-ts-exchange-rates/index.html"
+  },
+  {
+    "revision": "0263b0c61fc9ada7cfeb",
+    "url": "/react-ts-exchange-rates/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d5ba6a8644b822fa7a6",
+    "url": "/react-ts-exchange-rates/static/js/2.471e187d.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/react-ts-exchange-rates/static/js/2.471e187d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0263b0c61fc9ada7cfeb",
+    "url": "/react-ts-exchange-rates/static/js/main.2692c68e.chunk.js"
+  },
+  {
+    "revision": "25f6a8d4b1af94b960f6",
+    "url": "/react-ts-exchange-rates/static/js/runtime-main.73f56f11.js"
+  }
+]);
